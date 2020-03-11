881c67e4ad9a2c3ab998003dfc523dd79b2e083c
SanjayaWisnu33 <sanjayawisnu16@gmail.com>
2020-03-11 22:25:37 +0700

    scrap

--- Jason.py
+++ Jason.py
@@ -2,6 +2,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import datetime
 
 
 my_dict = {}
@@ -10,12 +11,15 @@ page = requests.get("https://www.republika.co.id/")
 obj = BeautifulSoup(page.text, 'html.parser')
 all = []
 for headline in obj.find_all('div',class_='teaser_conten1'):
-    x = json.dumps(headline.find('h1').text)
-    y = json.dumps(headline.find('h2').text)
-    z = json.dumps(headline.find('div', class_='date').text)
+    currentDT = datetime.datetime.now()
+    x = (headline.find('h1').text)
+    y = (headline.find('h2').text)
+    z = (headline.find('div', class_='date').text)
+    t = currentDT.strftime("%d %b %Y %H:%M:%S")
     my_dict['Kategori']=x
     my_dict['Judul']=y
-    my_dict['Waktu']=z    
+    my_dict['Waktu']=z
+    my_dict['Waktu_Publish']= t    
     
     all.append( dict(my_dict))    
     
